c36b0f46c0d2855e79893a29f7ba9eebd3d2f9e2
CMulatti <ca.mulatti@duocuc.cl>
2026-05-15 19:32:43 -0400

    initial commit orderservice

---
+++ src/test/java/com/smartlogix/orderservice/OrderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlogix.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
